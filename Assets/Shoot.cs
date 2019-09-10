8050ac4ffaf99ad3e6ed9d41444c628cc3ff65ff
Jiening <Jiening.tao@loop.colum.edu>
2019-09-10 16:55:30 -0500

    created turret that shoots on mouse click, auto aim

---
+++ Assets/Shoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Bullet;
+
+    [SerializeField]
+    private Transform BulletSpawnLocation;
+
+    [SerializeField]
+    private float ShotSpeed;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject NewBullet =Instantiate(Bullet, BulletSpawnLocation.position,transform.rotation);
+            Rigidbody NewBulletRB = NewBullet.GetComponent<Rigidbody>();
+            Transform NewBulletT = NewBullet.GetComponent<Transform>();
+            NewBulletRB.velocity = NewBulletT.up * -1 * ShotSpeed;
+
+        }
+    }
+}
---
+++ Assets/Targeting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeting : MonoBehaviour
+{
+    [SerializeField]
+    private Transform TargetTransform;
+
+    [SerializeField]
+    private float speed;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = Vector3.RotateTowards(transform.position, TargetTransform.position, 10, 10);
+
+        Vector3 targetDir = TargetTransform.position - transform.position;
+
+        float step = speed * Time.deltaTime;
+
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+        Debug.DrawRay(transform.position, newDir, Color.red);
+
+        // Move our position a step closer to the target.
+        transform.rotation = Quaternion.LookRotation(newDir);
+
+    }
+}
